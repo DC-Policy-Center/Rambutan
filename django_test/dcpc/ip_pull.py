ef1c1766e85aaeaea1fce91ae30ad35b531f627f
Michael Watson <michael@dcpolicycenter.org>
2017-11-07 14:56:24 -0500

    updating django test

--- django_test/dcpc/ip_pull.py
+++ django_test/dcpc/ip_pull.py
@@ -1,21 +1,24 @@
 import subprocess
 
 def get_hostname_ip():
-	ip_raw = subprocess.check_output(["hostname","-I"])
-	ip_byte = ip_raw.decode()
+	try:
+		ip_raw = subprocess.check_output(["hostname","-I"])
+		ip_byte = ip_raw.decode()
 
-	ip_list = []
-	i = 0
-	do = True
-	while do ==True:
-		if ip_byte[i] == ' ':
-			do = False
-			break
-		else:
-			ip_list.append(ip_byte[i])
-		i += 1
-	ip = "".join(ip_list)
-	#print(ip_list)
-	#print()
-	#print(ip)
+		ip_list = []
+		i = 0
+		do = True
+		while do ==True:
+			if ip_byte[i] == ' ':
+				do = False
+				break
+			else:
+				ip_list.append(ip_byte[i])
+			i += 1
+		ip = "".join(ip_list)
+		#print(ip_list)
+		#print()
+		#print(ip)
+	except:
+		ip = '0.0.0.0'
 	return(ip)
