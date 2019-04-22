9d434dfd47c95230a4269c7a7718286c004e386c
Michael Watson <mwatson10@gmail.com>
2019-04-22 14:11:14 -0400

    download file change for chunks

--- Waiau/dc-open-data/data-pull.py
+++ Waiau/dc-open-data/data-pull.py
@@ -13,6 +13,19 @@ def read_pull_list():
             data_list.append(row)
     return(data_list)
 
+
+def download_file(url,file_name):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(file_name, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+                    # f.flush()
+    return(file_name)
+
+
 def request_and_download(url,file_name):
     response = requests.get(url)
     if response.status_code != 200:
@@ -26,10 +39,12 @@ def request_and_download(url,file_name):
     return('Valid response, written')
 
 
+
+
 data_to_pull = read_pull_list()
 
 
-save_path = "~/Documents/data"
+save_path = "../data"
 
 i = 0
 for i in range(len(data_to_pull)):
@@ -39,6 +54,6 @@ for i in range(len(data_to_pull)):
     url = data_to_pull[i]['endpoint']
     full_file_name = os.path.join(save_path,file_name)
 
-    request_and_download(url,full_file_name)
+    download_file(url,full_file_name)
 
     i+=1
